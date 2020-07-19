890222fbace18cc157e7866e0e9fb2c7d69cada3
Brian Schulte <brianr.schulte@gmail.com>
2020-07-19 10:07:31 -0400

    added morgan logging

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import * as morgan from 'morgan';
+
 import { UserModule } from './modules/user/user.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { EmailModule } from './modules/email/email.module';
+import { Logger } from './modules/logger/logger';
 
 @Module({
   imports: [
@@ -22,4 +25,18 @@ import { EmailModule } from './modules/email/email.module';
     EmailModule,
   ],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  private morganLogger = new Logger('Morgan');
+
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(
+        morgan('combined', {
+          stream: {
+            write: str => this.morganLogger.verbose(str.replace(/\n/g, '')),
+          },
+        }),
+      )
+      .forRoutes('*');
+  }
+}
